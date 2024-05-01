b356c62691aef683194d6c316c162b0c47d03b87
Akita67 <l.bijman@student.maastrichtuniversity.nl>
2024-05-01 19:14:35 +0200

    Cars can finally move backwards to charge
    Change the code so that cars need to find a charging station before arriving at the end state
    When car arrives at the charging station just call the shortestPath function again and then concatenate the path from before the charging station with the one after and give that to the gameManager

--- iterative/src/Graph.java
+++ iterative/src/Graph.java
@@ -16,6 +16,7 @@ class Graph {
 
     public void addEdge(Vertex source, Vertex destination, double weight) {
         edges.add(new Edge(source, destination, weight));
+        edges.add(new Edge(destination, source, weight));
     }
 
     public List<Vertex> shortestPath(Vertex start, Vertex end, Agent agent, List<Agent> agents) {
@@ -23,6 +24,7 @@ class Graph {
         Map<Vertex, Vertex> previous = new HashMap<>();
         PriorityQueue<Vertex> priorityQueue = new PriorityQueue<>(Comparator.comparingDouble(time::get));
         boolean flag = false;
+        List<Vertex> temp = new ArrayList<>();
 
         for (Vertex vertex : vertices) {
             if (vertex == start) {
@@ -34,28 +36,36 @@ class Graph {
             previous.put(vertex, null);
         }
         while (!priorityQueue.isEmpty()) {
-            Vertex current = priorityQueue.poll();
-            if (current == end && !agent.did_charge) {
-                time.put(end, Double.MAX_VALUE);
+            Vertex current = priorityQueue.poll(); // takes the smallest
+            double newDistance = time.get(current);
+            //System.out.println(current.id);
+
+            if(current.charging_station && !agent.did_charge){
+                flag = true;
+                GameManager gameManager = new GameManager();
+                double waiting = gameManager.getTotalWaitingTime(agents,agent);
+                double charging_t = gameManager.getChargingTime(agent,current);
+                newDistance += waiting + charging_t;
+                temp = shortestPath(current,end,agent,agents);
+                break;
             }
+            if (current == end && !agent.did_charge) {
+                newDistance = Double.MAX_VALUE;
+            }
+            if(current == end && agent.did_charge){
+                break;
+            }
+
             List<Edge> edges = getOutgoingEdges(current);
             for (Edge neighbor : edges) {
-                double newDistance = time.get(current);
-                double waiting = 0;
-                double charging_t = 0;
-
-                if(current.charging_station && !agent.did_charge){
-                    flag = true;
-                    GameManager gameManager = new GameManager();
-                    waiting = gameManager.getTotalWaitingTime(agents,agent);
-                    charging_t = gameManager.getChargingTime(agent,current);
-                }
-
-                newDistance = time.get(current) + neighbor.weight + waiting + charging_t;
-
                 // allows to not go back to already previously seen node (if the weights are higher than before)
-                if (newDistance < time.get(neighbor.destination)) {
-                    time.put(neighbor.destination, newDistance);
+                //System.out.println("the destination " + neighbor.destination.getId());
+                double edgeWeight = newDistance + neighbor.getWeight();
+                if(neighbor.destination==end && !agent.did_charge){
+                    edgeWeight = Double.MAX_VALUE;
+                }
+                if (edgeWeight < time.get(neighbor.destination) ) { //TODO The car charged but now it can't come back because it was shorter to not charge
+                    time.put(neighbor.destination, edgeWeight);
                     previous.put(neighbor.destination, current);
                     priorityQueue.add(neighbor.destination);
                 }
@@ -64,6 +74,9 @@ class Graph {
         // Reconstruct the path
         List<Vertex> path = new ArrayList<>();
         Vertex current = end;
+        if(temp.size()>0){
+            current = temp.get(0);
+        }
         while (current != null) {
             path.add(current);
             current = previous.get(current);
@@ -74,6 +87,18 @@ class Graph {
             agent.did_charge=false;
             agent.charging_station=null;
             agent.charging_time=0;
+        }
+        //System.out.println("this is the path size " + path.size() );
+        for(Vertex a : path){
+            //System.out.println(a.getId());
+        }
+        if(temp.size()>0){
+            //System.out.println("this is the temp size " + temp.size());
+            path.remove(path.size()-1);
+        }
+        for(Vertex t : temp){
+            path.add(t);
+            //System.out.println(t.getId());
         }
 
         return path;
--- iterative/src/Main.java
+++ iterative/src/Main.java
@@ -11,8 +11,8 @@ public class Main {
         Vertex v1 = new Vertex(1);
         Vertex v2 = new Vertex(2);
         Vertex v3 = new Vertex(3);
-        Vertex v4 = new Vertex(4);v4.charging_station=true;
-        Vertex v5 = new Vertex(5);
+        Vertex v4 = new Vertex(4);
+        Vertex v5 = new Vertex(5);v4.charging_station=true;
         Vertex v6 = new Vertex(6);
 
 
